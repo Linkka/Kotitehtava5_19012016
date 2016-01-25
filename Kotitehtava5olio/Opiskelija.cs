15c1bbcda2059a8cdf3f14c982aa15a60f2ee9fb
Linkka <samilindroos@hotmail.com>
2016-01-26 00:42:56 +0200

    Initial commit.

---
+++ Kotitehtava5olio/Opiskelija.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kotitehtava5olio
+{
+    class Opiskelija
+    {
+        // properties
+        public string Etunimi { get; set; }
+        public string Sukunimi { get; set; }
+        public int Ika { get; set; }
+
+        // don't create any constructor, so default one will be used
+
+        // method to give more speed
+        public void Ikaanny()
+        {
+            Ika += 1;
+        }
+
+        // method to display car data
+        public void PrintData()
+        {
+            Console.WriteLine("Opiskelijan tiedot : ");
+            Console.WriteLine("- Etunimi : " + Etunimi);
+            Console.WriteLine("- Sukunimi : " + Sukunimi);
+            Console.WriteLine("- Ika : " + Ika);
+        }
+
+        // destructor
+        ~Opiskelija()
+        {
+            Console.WriteLine("Opiskelija object destroyed.");
+        }
+
+    }
+}
---
+++ Kotitehtava5olio/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kotitehtava5olio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // luo opiskelijat
+            Opiskelija opiskelija1 = new Opiskelija();
+            opiskelija1.Etunimi = "Sami";
+            opiskelija1.Sukunimi = "Lindroos";
+            opiskelija1.Ika = 20;
+
+            Opiskelija opiskelija2 = new Opiskelija();
+            opiskelija2.Etunimi = "Mikko";
+            opiskelija2.Sukunimi = "Mallikas";
+            opiskelija2.Ika = 15;
+
+            Opiskelija opiskelija3 = new Opiskelija();
+            opiskelija3.Etunimi = "Maija";
+            opiskelija3.Sukunimi = "Meikalainen";
+            opiskelija3.Ika = 25;
+            // display car data
+            opiskelija1.PrintData();
+            opiskelija2.PrintData();
+            opiskelija3.PrintData();
+            Console.ReadLine();
+            // accelerate, change car's speed value
+            opiskelija1.Ikaanny();
+            opiskelija2.Ikaanny();
+            opiskelija3.Ikaanny();
+            // display car data again, notice changed speed value!
+            opiskelija1.PrintData();
+            opiskelija2.PrintData();
+            opiskelija3.PrintData();
+            Console.ReadLine();
+        }
+    }
+
+}
